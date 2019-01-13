7ce183d36dc528878f7d24867614fa00c7ba942a
bells <bellszhu@gmail.com>
2019-01-13 13:12:16 +0800

    seven reverse integer

---
+++ leetcode/src/main/java/bellszhu/leetcode/easy/ReverseInteger7.java
@@ -0,0 +1,28 @@
+package bellszhu.leetcode.easy;
+
+public class ReverseInteger7 {
+
+    public static void main(String[] args) {
+        System.out.println(reverse(-123));
+    }
+
+    public static int reverse(int x) {
+        long result = 0;
+        int sign = 1;
+        if (x < 0) {
+            sign = -1;
+            x = -x;
+        }
+
+        while (x > 0) {
+            int remainder = x % 10;
+            result = result * 10 + remainder;
+            x = x / 10;
+        }
+        if (result > Integer.MAX_VALUE) {
+            return 0;
+        }
+
+        return (int)result * sign;
+    }
+}
